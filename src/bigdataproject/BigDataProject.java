8d860e329199f9a7b0be8b027ac5402988c4b54a
t04st3r <raffaele.tosti@studenti.unipg.it>
2016-04-25 15:31:00 +0800

    implemented centroid calculation class

--- src/bigdataproject/BigDataProject.java
+++ src/bigdataproject/BigDataProject.java
@@ -34,30 +34,33 @@ public class BigDataProject {
             Instance tmp = new DenseInstance(read.matrix[i]);
             ds.add(tmp);
         }
-        for (int i = 2; i < 30; i++) {
-            Clusterer km = new KMeans(i);
-            Dataset[] clusters = km.cluster(ds);
-            System.out.println("Length: " + clusters.length);
-            ClusterEvaluation aic = new AICScore();
-            ClusterEvaluation bic = new BICScore();
-            ClusterEvaluation sse = new SumOfSquaredErrors();
-            ClusterEvaluation saps = new SumOfAveragePairwiseSimilarities();
-            double aicScore = aic.score(clusters);
-            double bicScore = bic.score(clusters);
-            double sseScore = sse.score(clusters);
-            double sapsScore = saps.score(clusters);
-            System.out.println("AIC Score: " + aicScore);
-            System.out.println("BIC Score: " + bicScore);
-            System.out.println("SSE Score: " + sseScore);
-            System.out.println("SAPS Score: " + sapsScore);
-        }
-//        for (int i = 0; i < clusters.length; i++) {
-//            int index = i + 1;
-//            System.out.println("Cluster: " + index);
-//            for (Instance temp : clusters[i]) {
-//                System.out.println(temp);
-//            }
+//        for (int i = 2; i < 30; i++) {
+//            Clusterer km = new KMeans(i);
+//            Dataset[] clusters = km.cluster(ds);
+//            System.out.println("Length: " + clusters.length);
+//            ClusterEvaluation aic = new AICScore();
+//            ClusterEvaluation bic = new BICScore();
+//            ClusterEvaluation sse = new SumOfSquaredErrors();
+//            ClusterEvaluation saps = new SumOfAveragePairwiseSimilarities();
+//            double aicScore = aic.score(clusters);
+//            double bicScore = bic.score(clusters);
+//            double sseScore = sse.score(clusters);
+//            double sapsScore = saps.score(clusters);
+//            System.out.println("AIC Score: " + aicScore);
+//            System.out.println("BIC Score: " + bicScore);
+//            System.out.println("SSE Score: " + sseScore);
+//            System.out.println("SAPS Score: " + sapsScore);
 //        }
+        Clusterer km = new KMeans(10);
+        Dataset[] clusters = km.cluster(ds);
+        for (int i = 0; i < clusters.length; i++) {
+            int index = i + 1;
+            DatasetToMatrix sdm = new DatasetToMatrix(clusters[i]);
+            System.out.println("\nCluster "+index+" Number of samples: "+sdm.samples+"\n");
+            sdm.printMatrix();
+            sdm.printCentroid();
+            
+        }
 
     }
 
